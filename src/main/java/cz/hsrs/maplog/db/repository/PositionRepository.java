82f74ce7d75b79af01a2f6f5f0704251b987f05c
OKaas <kaas@ntis.zcu.cz>
2017-06-12 12:04:24 +0200

    - removing unnecessary data repository

--- src/main/java/cz/hsrs/maplog/db/repository/PositionRepository.java
+++ src/main/java/cz/hsrs/maplog/db/repository/PositionRepository.java
@@ -14,7 +14,7 @@ public interface PositionRepository extends CrudRepository<PositionEntity, Long>
     /* --- Collaborates --- */
 
     /* --- Getters / Setters --- */
-    
+
     /* --- Commons  --- */
 }
 
